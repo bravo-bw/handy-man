3769a13115ce2c4f7267281320c1805a2ee6b153
Tshepiso Setsiba <tsetsiba@bhp.org.bw>
2016-04-21 00:13:43 +0200

    add functionality to job allocation

--- handy_man/apps/job/views/job_allocation_view.py
+++ handy_man/apps/job/views/job_allocation_view.py
@@ -3,6 +3,8 @@ from django.shortcuts import render_to_response
 from django.template import RequestContext
 
 from handy_man.apps.main.views.base_dashboard import BaseDashboard
+from handy_man.apps.job.models.job import Job
+from handy_man.apps.user_profile.models.profile import UserProfile
 
 
 class JobAllocationView(BaseDashboard):
@@ -11,6 +13,7 @@ class JobAllocationView(BaseDashboard):
 
     def __init__(self):
         self.context = {}
+        self._job_identifier = None
         super(JobAllocationView, self).__init__()
 
     def get(self, request, *args, **kwargs):
@@ -20,16 +23,38 @@ class JobAllocationView(BaseDashboard):
         return render_to_response(self.template_name, self.context, context_instance=RequestContext(request))
 
     def post(self, request, *args, **kwargs):
-        print("job_id:", request.POST.get('job_id'))
-        messages.success(request, "Successfully assigned to setsiba Tshepiso.")
-#         job_allocation_form = JobAllocationForm(request.POST or None)
-#         if job_allocation_form.is_valid():
-#             instance = job_allocation_form.save(commit=False)
-#             instance.save()
-#             messages.success(request, "Successfully assigned to setsiba Tshepiso.")
-#         else:
-#            messages.success(request, "Successfully assigned to setsiba Tshepiso.") 
+        self._job_identifier = request.POST.get('job_id')
+        self._user_id = request.POST.get('artisan')
+        messages.success(request, "Job {} has been allocated to {}")
+        if self.assign_job():
+            messages.success(request, "Job {} has been allocated to {}".format(self.job, self.user_profile))
+        else:
+            messages.success(request, "Failed to allocate job to artisan.")
         self.context.update({
             'title': self.title,
         })
         return render_to_response(self.template_name, self.context, context_instance=RequestContext(request))
+
+    @property
+    def job(self):
+        try:
+            job = Job.objects.get(identifier=self._job_identifier)
+        except Job.DoesNotExist:
+            pass
+        return job
+
+    @property
+    def user_profile(self):
+        try:
+            user_profile = UserProfile.objects.get(user__id=self._user_id)
+        except UserProfile.DoesNotExist:
+            pass
+        return user_profile
+
+    def assign_job(self):
+        if self.job:
+            job = self.job
+            job.allocated_to = self.user_profile
+            job.save()
+            return True
+        return False
