58b2775aa7f5d9a47cce0507706ba0921aa18f4e
SirOne Pharatlhatlhe <sirpharatlhatlhe@yahoo.co.uk>
2016-04-26 08:11:05 +0200

    classes

---
+++ handy_man/apps/user_profile/classes/menu_configuration.py
@@ -0,0 +1,16 @@
+from ...main.constants import MY_PROFILE, HOME, ARTISAN, CUSTOMER, SME, JOBS, POST_JOB, USERS
+
+
+class MenuConfiguration:
+
+    # TODO: Consider making these configurable as database records, but be carefull of performance penalty of having
+    # to query DB for menus at every request to server
+    def user_menu_list(self, user_profile):
+        if not user_profile.account_type:
+            return [HOME, MY_PROFILE]
+        elif user_profile.account_type == ARTISAN:
+            return [HOME, MY_PROFILE, JOBS, USERS]
+        elif user_profile.account_type == CUSTOMER:
+            return [HOME, MY_PROFILE, USERS, POST_JOB]
+        elif user_profile.account_type == SME:
+            [HOME, MY_PROFILE, JOBS, USERS, POST_JOB]
