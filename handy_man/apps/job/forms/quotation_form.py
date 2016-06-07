0d8034f72a9011d4ca9ef1689bc045a0acf9e26b
SirOne Pharatlhatlhe <sirpharatlhatlhe@yahoo.co.uk>
2016-06-07 22:59:25 +0200

    fixed a few issues from merge

--- handy_man/apps/job/forms/quotation_form.py
+++ handy_man/apps/job/forms/quotation_form.py
@@ -1,6 +1,7 @@
 from django import forms
 
 from handy_man.apps.main.choices import CURRENCY
+from handy_man.apps.user_profile.models import UserProfile
 
 from ..models import Quote, Job
 
@@ -13,7 +14,8 @@ class QuotationForm(forms.ModelForm):
     amount = forms.DecimalField(widget=forms.widgets.NumberInput(attrs={'placeholder': 'Amount'}))
     accepted = forms.CheckboxInput()
     job = forms.ModelChoiceField(queryset=Job.objects.all(), to_field_name='pk')
+    artisan = forms.ModelChoiceField(queryset=UserProfile.objects.all(), to_field_name='pk')
 
     class Meta:
         model = Quote
-        fields = ['currency', 'estimate_hours', 'rate_per_hour', 'amount', 'job', 'accepted']
+        fields = ['currency', 'estimate_hours', 'rate_per_hour', 'amount', 'job', 'accepted', 'artisan']
