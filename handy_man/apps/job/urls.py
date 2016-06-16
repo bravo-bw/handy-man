1cd40bb95db2ea7c09713ad82f01ca0c5a54f40b
Coulson <ckgathi@bhp.org.bw>
2016-06-16 21:09:15 +0200

    urls fix

--- handy_man/apps/job/urls.py
+++ handy_man/apps/job/urls.py
@@ -7,7 +7,7 @@ from .views import (
 urlpatterns = [
     url(r'^job_posting/', JobPostingView.as_view(), name='job_posting_url'),
     url(r'^job_allocation/', JobAllocationView.as_view(), name='jobs_url'),
-    url(r'^job_quote/(?P<job>[0-9]{1,4})/$', JobQuotationsView.as_view(), name='quote_url'),
+    url(r'^job_quote/(?P<job>.*)/$', JobQuotationsView.as_view(), name='quote_url'),
     url(r'^available_jobs/', JobInterestView.as_view(), name='available_url'),
     url(r'^job_timeline/(?P<job_identifier>.*)/', JobTimelineView.as_view(), name='job_timeline_url'),
     url(r'^job_display/(?P<job_identifier>.*)/', JobDisplayView.as_view(), name='job_display_url'),
