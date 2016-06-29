8fd39c0f83519bb2d4fc71455f1fd88716f80d60
Leslie <leslie.kep@gmail.com>
2016-06-30 01:08:16 +0200

    auto create permissions and add user types to groups

--- manage.py
+++ manage.py
@@ -6,5 +6,8 @@ if __name__ == "__main__":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "handy_man.settings")
 
     from django.core.management import execute_from_command_line
+    
+    if len(sys.argv) == 2 and sys.argv[1] == 'migrate':
+        execute_from_command_line(['manage.py', 'populate_groups'])
 
     execute_from_command_line(sys.argv)
