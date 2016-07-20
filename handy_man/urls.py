2497e11c4b1c106080f51fa58b40033a56406a71
Tshepiso Setsiba <tsetsiba@bhp.org.bw>
2016-07-20 21:39:26 +0200

    ignore static files

--- handy_man/urls.py
+++ handy_man/urls.py
@@ -29,3 +29,10 @@ urlpatterns = [
 urlpatterns += patterns(
     url(r'^static/(?P<path>.*)$', serve),
 )
+
+if settings.DEBUG:
+    import debug_toolbar
+    urlpatterns += patterns(
+        '',
+        url(r'^__debug__/', include(debug_toolbar.urls)),
+    )
