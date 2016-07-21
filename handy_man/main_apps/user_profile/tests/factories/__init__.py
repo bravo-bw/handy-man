ba80a4da3a91565fe28552939745c06ec2cbab37
SirOne Pharatlhatlhe <sirpharatlhatlhe@yahoo.co.uk>
2016-07-21 22:59:23 +0200

    fixing a few imports

--- handy_man/main_apps/user_profile/tests/factories/__init__.py
+++ handy_man/main_apps/user_profile/tests/factories/__init__.py
@@ -1,2 +1,2 @@
-from .user_profile_factory import UserFactory, UserProfileFactory
 from .profession_factory import ProfessionFactory
+from .user_profile_factory import UserFactory, UserProfileFactory
